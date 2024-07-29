1207693574de546f5fc0850a9ebc13095a2d8b72
agustinellargo <agustinellargo@gmail.com>
2024-07-29 19:41:43 +0200

    primera subida

---
+++ Main.java
@@ -0,0 +1,23 @@
+import static java.lang.Math.random;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+        int[] array = new int[10];
+
+        for(int i = 0;i<array.length;i++){
+            int numAleatorio = (int) (Math.random() * 20) + 1;
+            if(array[i]== numAleatorio){
+
+            }
+            else if(array[i] !=numAleatorio){
+                array[i] = numAleatorio;
+            }
+            System.out.println(numAleatorio);
+            }
+        }
+
+    }
